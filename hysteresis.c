30f8dc5d9c02904d0360b69c8834e8cb5f5e0275
Remsko <pinoit.remi@gmail.com>
2019-09-04 21:55:04 +0200

    update

--- hysteresis.c
+++ hysteresis.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/02 21:24:01 by rpinoit           #+#    #+#             */
-/*   Updated: 2019/09/02 22:36:16 by rpinoit          ###   ########.fr       */
+/*   Updated: 2019/09/04 21:53:43 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,10 @@ unsigned int hysteresis(unsigned int new_percentage)
 		(t_transition){2, 35, 40},
 		(t_transition){3, 60, 65},
 		(t_transition){4, 85, 90}};
-	bool decrease;
 
-	decrease = (new_percentage < last_percentage) ? true : false;
-	level = 0;
-	if (decrease == true)
+	if (new_percentage < last_percentage)
 	{
+		level = 4;
 		for (int i = TABLE_LENGTH - 1; i >= 0; i--)
 		{
 			if (new_percentage <= table[i].down)
@@ -37,6 +35,7 @@ unsigned int hysteresis(unsigned int new_percentage)
 	}
 	else
 	{
+		level = 0;
 		for (int i = 0; i < TABLE_LENGTH; i++)
 		{
 			if (new_percentage >= table[i].up)
--- main.c
+++ main.c
@@ -7,5 +7,14 @@ int main(int ac, char **av)
 	printf("5 => lvl: %u\n", hysteresis(5));
 	printf("25 => lvl: %u\n", hysteresis(25));
 	printf("15 => lvl: %u\n", hysteresis(15));
+	printf("100 => lvl: %u\n", hysteresis(100));
+	printf("100 => lvl: %u\n", hysteresis(100));
+	printf("0 => lvl: %u\n", hysteresis(0));
+	printf("0 => lvl: %u\n", hysteresis(0));
+
+	printf("5 => lvl: %u\n", hysteresis(5));
+	printf("91 => lvl: %u\n", hysteresis(91));
+	printf("86 => lvl: %u\n", hysteresis(86));
+
 	return (0);
 }
